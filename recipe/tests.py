6c2cea54990d70d2d390e7ddd3114ead25340ec9
xvysot00 <xvysot00@vutbr.cz>
2026-05-21 14:53:09 +0300

    test: add unit tests for models and views in recipe app

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Category, Recipe
 
-# Create your tests here.
+class RecipeAppTests(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Тестова категорія")
+        self.recipe = Recipe.objects.create(
+            title="Тестовий рецепт",
+            description="Опис тестового рецепта",
+            ingredients="Інгредієнти",
+            instructions="Інструкції",
+            category=self.category
+        )
+
+    def test_category_creation(self):
+        self.assertEqual(self.category.name, "Тестова категорія")
+        self.assertEqual(str(self.category), "Тестова категорія")
+
+    def test_recipe_creation(self):
+        self.assertEqual(self.recipe.title, "Тестовий рецепт")
+        self.assertEqual(self.recipe.category.name, "Тестова категорія")
+
+    def test_main_view_status_code_and_template(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main.html')
+        self.assertContains(response, "Тестовий рецепт")
+
+    def test_category_list_view_status_code_and_template(self):
+        response = self.client.get(reverse('category_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'category_list.html')
+        self.assertContains(response, "Тестова категорія")
