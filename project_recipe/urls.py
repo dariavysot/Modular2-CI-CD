f4d6b8d3c1bda279eacd8537cd5a8c8e1f918205
xvysot00 <xvysot00@vutbr.cz>
2026-05-21 14:48:35 +0300

    feat: add category_list view to display categories with recipe counts

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from recipe.views import main
+from recipe.views import main, category_list
 
 urlpatterns = [
-    path('admin/', admin.site.admin_site.urls),
+    path('admin/', admin.site.urls),
     path('', main, name='main'),
+    path('categories/', category_list, name='category_list'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,7 +1,11 @@
-
 from django.shortcuts import render
-from .models import Recipe
+from django.db.models import Count
+from .models import Recipe, Category
 
 def main(request):
     latest_recipes = Recipe.objects.order_by('-created_at')[:5]
     return render(request, 'main.html', {'latest_recipes': latest_recipes})
+
+def category_list(request):
+    categories = Category.objects.annotate(recipe_count=Count('categories'))
+    return render(request, 'category_list.html', {'categories': categories})
